509dda5b1f59aa30eb68aab71f3b0058f176daee
Megan Choy <megchoy3@gmail.com>
2021-05-06 08:48:29 -0700

    Main opens a window

--- Responsible Student Simulator/src/Main.java
+++ Responsible Student Simulator/src/Main.java
@@ -1,9 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main {
+public class Main extends JFrame {
+	
+	public Main(String title) {
+		super(title);
+		setBounds(100, 100, 800, 600);
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	    setVisible(true);
+	    
 	}
 
+	public static void main(String[] args)
+	{
+		Main w = new Main("Responsible Student Simulator");
+	}
 }
