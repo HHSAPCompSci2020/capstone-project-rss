8a4aa530bdc0c38ee34e31440e0afa30b1ea98ca
Jeremie Park <69928676+jeremiepark@users.noreply.github.com>
2021-05-07 09:13:16 -0700

    created braincell class

---
+++ Responsible Student Simulator/src/BrainCell.java
@@ -0,0 +1,34 @@
+
+public class BrainCell {
+	
+	double brainCellRate = 0;
+	double totalBrainCells = 0;
+	double prestigeLevel = 0;
+	
+	public BrainCell() {
+		brainCellRate = 0;
+		totalBrainCells = 0;
+		prestigeLevel = 0;
+	}
+	
+	public double getRate() {
+		return 0;
+	}
+	
+	public double getTotal() {
+		return 0;
+	}
+	
+	public double updateTotal() {
+		return 0;
+	}
+	
+	public void addTotal() {
+		return;
+	}
+	
+	public void addRate() {
+		return;
+	}
+	
+}
