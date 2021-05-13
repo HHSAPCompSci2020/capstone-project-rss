30a1978898a445d0481ab68308fd59581443fcab
Megan Choy <megchoy3@gmail.com>
2021-05-13 16:11:21 -0700

    course math

--- Responsible Student Simulator/src/Course.java
+++ Responsible Student Simulator/src/Course.java
@@ -9,14 +9,32 @@ public class Course {
 	private int level;
 	private double baseProduction, baseCost;
 	private double currentProduction, currentCost;
+	private String name;
 	
-	public Course(double baseProduction, double baseCost) {
+	public Course(String name, double baseProduction, double baseCost, double time) {
 		this.baseCost = baseCost;
 		this.baseProduction = baseProduction;
 		level = 1;
 		currentProduction = baseProduction;
 		currentCost = baseProduction;
+		this.name = name;
 	}
 	
+	public void run() {
+		
+	}
 	
+	public void levelUp() {
+		currentCost = baseCost * Math.pow(1.15, level);
+		level++;
+		currentProduction = baseProduction * level;
+	}
+	
+	public double getProduction() {
+		return currentProduction;
+	}
+	
+	public double getCost() {
+		return currentCost;
+	}
 }
